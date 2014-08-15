336b0c70c2f1d7378f8a403399d6857cae01bd80
rxsegrxup <m.c.chase@spartans.nsu.edu>
2014-08-14 22:02:37 -0700

    cleanup

--- config.php
+++ config.php
@@ -3,7 +3,9 @@ $token = ""; //MyPlex auth token of myplex user below
 $username = ""; //username of the myplex user owning the PMS server that requests are proxied to
 $lbip = ""; //IP or hostname (No port or protocol) of the PMS server having the same ip as the Load Balancer
 $destserver = ""; //IP:port of the PMS server that requests are proxied to
+//$destserver = "localhost:32400" //If running RTT and PMS on the same server (use lan address if same lan)
 $destserverre = $destserver; //REMOTE IP:port of the PMS server that requests are proxied to
+//$destserverre = "REMOTEIP:REMOTEPORT" //Only use this if using lan ip for $destserver
 $lbname = ""; //Friendly name of the PMS server having the same ip as the Load Balancer
 $destname = ""; //Friendly name of the PMS server that requests are proxied to
 //Get the values below from the /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Preferences.xml
@@ -14,6 +16,7 @@ $lbuuidkvp = array(
 //Get the values below from the /var/lib/plexmediaserver/Library/Application Support/Plex Media Server/Preferences.xml
 //Server requests are being proxied to
 $destuuid = "ProcessedMachineIdentifier";
+$blocksections = TRUE; //DO NOT allow access to sections.
 $log = "/path/to/logs/changeup.log";
 $loggingenabled = TRUE;
 $loghistorylength = 5;
@@ -21,6 +24,4 @@ $maxlogsize = 1* 1024 * 1024; //1MB
 
 $streamsize=10485760; //5MB. RTT Client streaming size. Not yet documented
 $enablerttclientmode=FALSE;// RTT Client mode will stream media instead of redirecting.
-//do not modify
-$unm = "";
 ?>
--- index.php
+++ index.php
@@ -2,205 +2,32 @@
 //v1.0
 try{
     require 'config.php';
+    require 'functions.php';
 }
 catch (Exception $e){
-    error_log ( "Error with PMS Load Balancer in ".__FILE__." at line 3. Unable to get config");
+    error_log ( "Error with PMS Load Balancer in ".__FILE__." at line 3. Unable to get config and/or functions");
     exit;
 }
+
 $unm = "";
 $utoken = "";
 $uhtoken = "";
-
-if($loggingenabled && !touch($log)){
-    $loggingenabled = FALSE;
-}
-elseif($loggingenabled){
-    $logdir = dirname($log);
-    $logfilename = basename($log);
-    if(!is_int($loghistorylength)){
-        $tmp = $loghistorylength;
-        $loghistorylength = 5;
-        //resulting to default max length
-        logger("got incorrect log max lenth $tmp using default 5");
-        unset($tmp);
-    }
-    if(!is_int($maxlogsize)){
-        $maxlogsize = 1 * 1024 * 1024; //1MB
-    }
-    logger("=============================================");
-}
-
-function logger($data){
-    global $log,$loggingenabled,$maxlogsize;
-    if(!$loggingenabled){
-        return;
-    }
-    $today = date("Y-m-d H:i:s");
-    file_put_contents($log,"[$today]: $data\r\n",FILE_APPEND);
-    if(filesize($log) > $maxlogsize){ //set this in config. Default is 1 MB
-        logrotate();
-    }
-}
-
-function logrotate(){
-    global $logdir,$logfilename,$log,$loghistorylength;
-    $logfiles = scandir($logdir);
-    natsort($logfiles);
-    $logfiles = array_reverse($logfiles);
-    $postlog=array();
-    foreach ($logfiles as $file) {
-        if(stripos($file, $logfilename) !== FALSE && $file != $logfilename){
-            $clognum = str_replace($logfilename.".","",$file);
-            $clognum = intval($clognum);
-            $postlog[] = "Current log is $clognum";
-            if($clognum >= $loghistorylength){
-                unlink("$logdir/$file");
-                $postlog[] = "removing log file $logdir/$file";
-            }
-            elseif($clognum == 0 || $clognum == "0"){
-            }
-            else{
-                rename("$logdir/$file", "$log.".($clognum+1));
-                $postlog[]= "renaming log from $logdir/$file to $log.".($clognum+1);
-            }
-        }
-    }
-    rename($log, "$log.1");
-    touch($log);
-    foreach($postlog as $plogl){
-        logger($plogl);
-    }
-}
-/*
-//In my tests I found that the client identifier doesnt much matter.
-//Only the username and token needs to be changed
-function replaceCookie($value, $repl){
-    $cookies = explode(";",$value);
-    $toreplace = "";
-    foreach ($cookies as $cookie) {
-        $kvp = explode("=", trim($cookie));
-        if($kvp[0] == "SESSION-GUID"){
-            $toreplace = $kvp[1];
-            break;
-        }
-    }
-    if($toreplace != ""){
-        return str_replace($toreplace, $repl, $value);
-    }
-    else{
-        return $value;
-    }
-}
-*/
-
-function fixRequestUri($uri){
-    global $token, $username, $lbip, $destserver, $unm,$utoken;//, $client
-    $uriparts = explode("?", $uri);
-    if(count($uriparts)>1){
-        $newuri = $uriparts[0]."?";
-        $uriq = $uriparts[1];
-    }
-    else{
-        return $uri;
-    }
-
-    $queryparts = explode("&", $uriq);
-    $firstrun = TRUE;
-    foreach ($queryparts as $query) {
-        $kvp = explode("=", trim($query));
-        switch ($kvp[0]) {
-            case 'X-Plex-Token':
-                $utoken = $kvp[1];
-                logger("found user token $utoken");
-                $kvp[1] = $token;
-                logger("fixing url part ".$kvp[0]."=".$kvp[1]);
-            break;
-            case 'X-Plex-Username':
-                $unm = $kvp[1];
-                logger("found username $unm");
-                $kvp[1] = $username;
-                logger("fixing url part ".$kvp[0]."=".$kvp[1]);
-            break;
-            case 'Referer':
-            case 'Host':
-            case 'Origin':
-                $kvp[1] = str_replace($lbip,$destserver, $kvp[1]);
-                logger("fixing url part ".$kvp[0]."=".$kvp[1]);
-            break;
-            /*
-              //In my tests I found that the client identifier doesnt much matter.
-              //Only the username and token needs to be changed
-            case 'Cookie':
-                $value = replaceCookie($value, $client);
-            break;
-            case 'X-Plex-Client-Identifier':
-                $value = $client;
-            break;*/
-        }
-        if($firstrun === TRUE){
-            $firstrun = FALSE;
-            $newuri .= $kvp[0]."=".$kvp[1];
-        }
-        else{
-            $newuri .= "&".$kvp[0]."=".$kvp[1];
-        }
-    }
-    return $newuri;
-}
-
-function getUsername($userheaders){
-    $ch = curl_init("https://plex.tv/users/account");
-    curl_setopt($ch,CURLOPT_ENCODING, '');
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_VERBOSE,1);
-    curl_setopt($ch, CURLOPT_HEADER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $userheaders);
-    $output = curl_exec($ch);
-    curl_close($ch);
-    if(strpos($output,"<username>") != FALSE){
-        $st = strpos($output,"<username>") + strlen("<username>");
-        $ed = strpos($output,"</username>");
-        $usernm=substr($output,$st,($ed-$st));
-        logger("found username $usernm");
-        return $usernm;
-    }
-    else{
-        logger("Username tag not found");
-        return "";
-    }
-
-}
+$logdir = "./";
+setupLogging();
 
 $headers = array();
 $userheaders = array();
 $accepteh=FALSE;
 $acceptr=FALSE;
 
-
-
-//logger($_SERVER['REQUEST_METHOD']);
-//logger($_SERVER['REQUEST_URI']);
 $ouri = $_SERVER['REQUEST_URI'];
 $originalurl = "http://$destserver$ouri";
-if( stripos($ouri, "/:/prefs") !== FALSE ||
-    (stripos($ouri, "/:/plugins") !== FALSE && stripos($ouri, "/prefs") !== FALSE) ||
-    stripos($ouri, "/status/sessions") !== FALSE){
-    //block
-       // /:/plugins/<packagename>/prefs
-       // /:/prefs
-       // status/sessions
-    //allow
-      //  /video/<channel>
-      //  /music/<channel>
-      //  /photo/<channel>
-    logger("blocking $ouri");
-    logger("http://$destserver$ouri");
-    header("Location: $originalurl");
-    exit;
-}
+
+checkBlockUrls($ouri, $originalurl);
+
 $ruri = fixRequestUri($ouri);
+
 $finalurl = "http://$destserver$ruri";
-//logger("http://$destserver$ruri");
 foreach (getallheaders() as $name => $value) {
     $ovalue = $value;
     switch ($name) {
@@ -225,28 +52,6 @@ foreach (getallheaders() as $name => $value) {
             $value = str_replace($lbip,$destserver, $value);
             logger("adding $name $value");
         break;
-        //With RTT Client you can instead stream media instead of redirecting
-        case 'Range':
-            if($enablerttclientmode && stripos($finalurl,"/library/parts/") !== FALSE){
-                logger("checking range $value");
-                $valsplit = explode("-",$value);
-                logger("checking range0 ".$valsplit[0]);
-                $valsplit[0] = explode("=",$valsplit[0])[1];
-                logger("checking range1 ".$valsplit[0]);
-                logger("checking range count ".count($valsplit));
-                if(count($valsplit) > 1 && intval($valsplit[1])-intval($valsplit[0])>$streamsize){
-                    logger('fixing range');
-                    $newend=intval($valsplit[0])+$streamsize;
-                    $value = "bytes=".$valsplit[0]."-$newend";
-                }
-                elseif(count($valsplit) == 1 || (count($valsplit) ==2 && $valsplit[1]=="") ){
-                    logger('fixing range');
-                    $newend=intval($valsplit[0])+$streamsize;
-                    $value = "bytes=".$valsplit[0]."-$newend";
-                }
-                logger("adding $name $value");
-            }
-        break;
         /*
           //In my tests I found that the client identifier doesnt much matter.
           //Only the username and token needs to be changed
@@ -266,13 +71,13 @@ foreach (getallheaders() as $name => $value) {
     $userheaders[] = "$name: $ovalue";
 }
 
-//To enable RTT Client mode so that you can instead stream media instead of redirecting
-if(!$enablerttclientmode && (stripos($finalurl,"/library/parts/") !== FALSE || stripos($finalurl,"/video/:/transcode/") !== FALSE) ){
+if(stripos($finalurl,"/library/parts/") !== FALSE || stripos($finalurl,"/video/:/transcode/") !== FALSE){
     $finalurlre = "http://$destserverre$ruri";
     header("Location: $finalurlre");
-    logger("Redirecting to redue load");
+    logger("Redirecting to reduce load");
     exit;
 }
+
 
 if($unm == "" && ($utoken != "" || $uhtoken != "" ) ){
     if($uhtoken == ""){
@@ -284,17 +89,9 @@ if($unm == "" && ($utoken != "" || $uhtoken != "" ) ){
     }
     logger("looking up username with token $utoken");
     $unm = getUsername($userheaders);
-    logger("changing username to $unm");
 }
 
-$ch = curl_init($finalurl);
-curl_setopt($ch,CURLOPT_ENCODING, '');
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_VERBOSE,1);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $_SERVER['REQUEST_METHOD']);
-$output = curl_exec($ch);
+$output = sendRequest($finalurl, $headers, $_SERVER['REQUEST_METHOD']);
 
 //Get the response code before closing curl
 $httpCode = curl_getinfo ( $ch, CURLINFO_HTTP_CODE );
@@ -308,11 +105,7 @@ $header = explode("\r\n",$header);
 http_response_code(intval($httpCode));
 $firstrun = TRUE;
 
-
-
-//logger($httpCode);
-//logger($_SERVER['REQUEST_URI']);
-//logger("http://$destserver$ruri");
+logger("Server response code $httpCode");
 foreach($header as $value){
     if($firstrun === TRUE){
         //The first header is always HTTP 1/1 CODE
@@ -325,33 +118,13 @@ foreach($header as $value){
         $accepteh=TRUE;
     }
     else if(stripos($value,$destserver) !== FALSE){
-        logger("Fixing ip");
+        logger("Fixing ip from $destserver to $lbip");
         $value = str_replace($destserver, $lbip, $value);
     }
     header("$value");
 }
 
-
-if(strpos($body,'friendlyName="'.$destname.'"') !== FALSE){
-    $body = str_replace('friendlyName="'.$destname.'"', 'friendlyName="'.$lbname.'"', $body);
-    logger("trying to swap back friendlyname $lbname");
-}
-if(strpos($body,'machineIdentifier="'.$destuuid.'"') !== FALSE){
-    $body = str_replace('machineIdentifier="'.$destuuid.'"', 'machineIdentifier="'.$lbuuidkvp[$unm].'"', $body);
-    logger("trying to swap back machineID ".$lbuuidkvp[$unm]);
-}
-if(strpos($body,'myPlexUsername="'.$username.'"') !== FALSE){
-    $body = str_replace('myPlexUsername="'.$username.'"', 'myPlexUsername="'.$unm.'"', $body);
-    logger("trying to swap back username $unm");
-}
-if(strpos($body,'username="'.$username.'"') !== FALSE){
-    $body = str_replace('username="'.$username.'"', 'username="'.$unm.'"', $body);
-    logger("trying to swap back username $unm");
-}
-if(strpos($body,'authToken="'.$token.'"') !== FALSE){
-    $body = str_replace('authToken="'.$token.'"', 'authToken="'.$utoken.'"', $body);
-    logger("trying to swap back token $utoken");
-}
+$body = swapBackBody($body);
 
 if(($accepteh === TRUE && $acceptr === TRUE)){
     echo gzencode($body);
